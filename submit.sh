37c7f1b561769a0eb52c5abbdc16bccecaf68888
Nil Stolt Ansó <nil@bureaudigiraat.nl>
2020-03-11 05:27:16 +0100

    changed sh fil

--- submit.sh
+++ submit.sh
@@ -6,11 +6,10 @@
 #SBATCH --mail-user=nil.stoltanso@student.uva.nl
 #SBATCH --output=name%j.out
 module purge
-module load eb
 
-module load Python/3.6.3-foss-2017b
-module load cuDNN/7.0.5-CUDA-9.0.176
-module load NCCL/2.0.5-CUDA-9.0.176
+module load Python/3.6.6-foss-2018b
+module load cuDNN/7.6.3-CUDA-10.0.130
+module load CUDA/10.0.130
 
 export LD_LIBRARY_PATH=/hpc/eb/Debian9/cuDNN/7.1-CUDA-8.0.44-GCCcore-5.4.0/lib64:$LD_LIBRARY_PATH
 srun python3 run.py
