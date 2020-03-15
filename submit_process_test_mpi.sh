7d22524a231a28d90d4598d51bba65e03e074935
Nil Stolt Ansó <nil@bureaudigiraat.nl>
2020-03-15 12:59:28 +0100

    Pressing buttons

--- submit_process_test_mpi.sh
+++ submit_process_test_mpi.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --time=0-00:5:00
+#SBATCH --time=0-00:10:00
 #SBATCH --mem=20000M
 #SBATCH --nodes=1
-#SBATCH --partition=short
+#SBATCH --partition=gpu_short
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=nil.stoltanso@student.uva.nl
 #SBATCH --output=name%j.out
@@ -15,4 +15,4 @@ module load Anaconda3/2018.12
 conda activate test
 
 export LD_LIBRARY_PATH=/hpc/eb/Debian9/cuDNN/7.1-CUDA-8.0.44-GCCcore-5.4.0/lib64:$LD_LIBRARY_PATH
-srun mpiexec -n 1 python3 run.py --envs_per_process=128 --nsteps_per_seg=128
+srun mpiexec -n 4 python3 run.py --envs_per_process=32 --nsteps_per_seg=128
